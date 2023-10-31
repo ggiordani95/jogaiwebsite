ce608c99f328acba0b8409cd78fc79d0e707fcec
Gustavo Giordani <94162641+ggiordani95@users.noreply.github.com>
2023-10-31 12:46:01 -0300

    new commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 import Logo from "$lib/assets/jogailogo.png"
-import mockup from "$lib/assets/mockupcel.png"
+import mockup from "$lib/assets/mockup2.png"
 import google from "$lib/assets/googleplay.png"
 import apple from "$lib/assets/appstore.png"
 import { onMount } from 'svelte';
@@ -17,18 +17,18 @@ import { fade, fly } from 'svelte/transition';
     {#if loaded}
     <div class="w-full my-12 flex flex-col justify-center items-center gap-32">
         <div class="w-[900px] flex flex-row justify-between items-center">
-            <img src={Logo} alt="jogai logo" in:fade={{delay: 150, duration: 2500}} />
-            <ul class="text-zinc-500 font-semibold flex flex-row gap-8 text-lg">
-                <li>
+            <img src={Logo} alt="jogai logo" in:fade={{delay: 150, duration: 2500}} class="hover:scale-110 transition-all"/>
+            <ul class="text-zinc-500 font-semibold flex flex-row gap-8 text-lg cursor-pointer">
+                <li class="hover:scale-110 hover:text-white">
                     Home
                 </li>
-                <li>
+                <li class="hover:scale-110 hover:text-white">
                     Serviços
-                </li>
-                <li>
+                </li >
+                <li class="hover:scale-110 hover:text-white">
                     Contato
                 </li>
-                <li class="text-yellow-300 underline underline-offset-4">
+                <li class="text-yellow-300 underline underline-offset-4 hover:scale-110">
                     Entrar
                 </li>
             </ul>
@@ -37,7 +37,7 @@ import { fade, fly } from 'svelte/transition';
         <div class="w-[900px] flex flex-row justify-between items-center" in:fade={{delay: 150, duration: 2000}}>
             
             <div class="flex flex-col flex-1 mb-24 " in:fly={{x: -300, delay: 150, duration: 1200}}>
-                <h1 class="text-zinc-600 text-6xl max-w-xl font-semibold hover:text-yellow-400 transition-all hover:scale-110">
+                <h1 class="text-zinc-600 text-6xl max-w-xl font-semibold transition-all hover:scale-110">
                     Sistema que <span class="text-zinc-400">aproxima</span> arenas
                     e jogadores
                 </h1>
@@ -47,12 +47,13 @@ import { fade, fly } from 'svelte/transition';
                 <div class="flex flex-row gap-1 mt-8 items-center">
                     <img src={google} alt="google logo" />
                     <img src={apple} alt="google logo" />
-                    <h4 class="text-white text-lg underline underline-offset-4 ml-4">Saiba mais</h4>
+                    <h4 class="text-white text-lg underline underline-offset-4 ml-4 cursor-pointer hover:text-yellow-400 hover:scale-110 transition-all">Saiba mais</h4>
                 </div>  
             </div>
             
             <div class="flex flex-col flex-1 justify-center items-end" in:fly={{x: 300, delay: 50, duration: 1300}}>
-                <img src={mockup} alt="jogai logo" width="240px" class="border-[0.3rem] border-gray-500 rounded-[3rem]" />
+                <img src={mockup} alt="jogai logo" width="240px"  />
+                
             </div>
             
         </div>
