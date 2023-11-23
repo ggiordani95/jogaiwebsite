63ab4b3478ca654f4a1add5cc8abc05353d7c43f
Gustavo Giordani <94162641+ggiordani95@users.noreply.github.com>
2023-11-23 19:01:30 -0300

    update

--- src/lib/ResourcesSection.svelte
+++ src/lib/ResourcesSection.svelte
@@ -6,15 +6,15 @@
         <div class="flex flex-row">
             <ul class="flex flex-row gap-12 justify-center">
                 <li>
-                    <h1 class="text-3xl font-semibold"><b><i>1</i></b> Gestão simplificada de reservas</h1>
+                    <h1 class="text-3xl font-semibold">Gestão simplificada de reservas</h1>
                     <p class="text-lg mt-4">Diga adeus aos sistemas complicados e falhas na comunicação. Nosso app oferece uma plataforma intuitiva e confiável para gerenciar suas reservas com eficiência e transparência.</p>
                 </li>
                 <li>
-                    <h1 class="text-3xl font-semibold"><b><i>2</i></b> Aumente a visibilidade da arena</h1>
+                    <h1 class="text-3xl font-semibold">Aumente a visibilidade da arena</h1>
                     <p class="text-lg mt-4">Ao se unir ao Jogaí, você fará parte de uma rede selecionada de arenas esportivas que se destacam pela qualidade e confiabilidade, atraindo ainda mais jogadores para suas quadras.</p>
                 </li>
                 <li>
-                    <h1 class="text-3xl font-semibold"><b><i>3</i></b> Receba feedback dos jogadores</h1>
+                    <h1 class="text-3xl font-semibold">Receba feedback dos jogadores</h1>
                     <p class="text-lg mt-4">Através de avaliações e feedbacks dos usuários, compreenda melhor as necessidades de seus clientes e aprimore continuamente a experiência oferecida em sua quadra.</p>
                 </li>
             </ul>
